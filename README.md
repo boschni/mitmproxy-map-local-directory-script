ce4f2ad1285d94f9f346350bc43a88a25961d31a
Niek Bosch <just.niek@gmail.com>
2017-07-27 17:33:29 +0200

    feat(): support multiple mappings

--- README.md
+++ README.md
@@ -5,11 +5,16 @@ This mitmproxy 2.0.2+ script will map requests to files on the local file system
 When running mitmproxy with the following arguments:
 
 ```bash
-mitmproxy --ignore '^(?!example\.com)' -s "/path/to/map_local_directory.py --path /url/to/statics/directory --src /projects/myproject/statics"
+mitmproxy -s "/path/to/map_local_directory.py --map http://site1.com/url/to/statics /projects/site1/statics http://site2.com/public/js /projects/site2/js"
 ```
 
-Requests like `http://example.com/url/to/statics/directory/js/somefile.js` will be mapped to `/projects/myproject/statics/js/somefile.js`.
-Any request that does not match the domain or with a file on the local file system will pass through unchanged.
+Requests like `http://site1.com/url/to/statics/js/somefile.js` will be mapped to `/projects/site1/statics/js/somefile.js` and requests like `http://site2.com/public/js/main.js` will be mapped to `/projects/site2/js/main.js`.
+Any request that does not match the url or with a file on the local file system will pass through unchanged.
+
+## Only process requests to example.com
+```bash
+mitmproxy --ignore '^(?!example\.com)' -s "/path/to/map_local_directory.py --map http://site1.com/url/to/statics /projects/site1/statics http://site2.com/public/js /projects/site2/js"
+```
 
 ## Enabling/disabling the OS-X proxy from the terminal
 Command to enable the OS-X proxy on the Wi-Fi interface and set it to localhost:8080:
--- map_local_directory.py
+++ map_local_directory.py
@@ -4,25 +4,31 @@ import mimetypes
 import urllib
 
 class Replacer:
-    def __init__(self, src, path):
-        self.src = src
-        self.path = path
+    def __init__(self, mappings):
+        self.mappings = mappings
+
+    def find_mapping(self, flow):
+        return next((m for m in self.mappings if flow.request.url.startswith(m[0])), None)
 
     def request(self, flow):
-        flow.request.anticache()
-        flow.request.anticomp()
+        mapping = self.find_mapping(flow)
+        if mapping is not None:
+            flow.request.anticache()
+            flow.request.anticomp()
 
     def response(self, flow):
-        relative_filepath = remove_prefix(flow.request.path.strip("/"), self.path.strip("/")).strip("/")
-        filepath = os.path.join(self.src, relative_filepath)
-        if os.path.isfile(filepath):
-            url = urllib.request.pathname2url(filepath)
-            mimetype = mimetypes.guess_type(url)[0]
-            with open(filepath, "r", encoding="utf-8", errors="ignore") as f:
-                flow.response.content = f.read().encode("utf-8")
-                flow.response.status_code = 200
-                flow.response.reason = "OK"
-                flow.response.headers["Content-Type"] = mimetype if mimetype is not None else "text/plain; charset=utf-8"
+        mapping = self.find_mapping(flow)
+        if mapping is not None:
+            path_after_url = remove_prefix(flow.request.url, mapping[0]).strip("/")
+            filepath = os.path.join(mapping[1], path_after_url)
+            if os.path.isfile(filepath):
+                url = urllib.request.pathname2url(filepath)
+                mimetype = mimetypes.guess_type(url)[0]
+                with open(filepath, "rb") as f:
+                    flow.response.content = f.read()
+                    flow.response.status_code = 200
+                    flow.response.reason = "OK"
+                    flow.response.headers["Content-Type"] = mimetype if mimetype is not None else "text/plain; charset=utf-8"
 
 def remove_prefix(text, prefix):
     if text.startswith(prefix):
@@ -31,7 +37,12 @@ def remove_prefix(text, prefix):
 
 def start():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--path", type=str, default="")
-    parser.add_argument("--src", type=str, default=".")
+    parser.add_argument("--map", nargs="*", type=str, default=[])
     args = parser.parse_args()
-    return Replacer(args.src, args.path)
+
+    mappings = []
+    for i in range(0, len(args.map)):
+        if i % 2 == 0:
+            mappings.append((args.map[i * 2], args.map[i * 2 + 1]))
+
+    return Replacer(mappings)
