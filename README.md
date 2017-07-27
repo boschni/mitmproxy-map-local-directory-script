f53e89151da9eb6891f75c9aa1dd5c457278ad84
Niek Bosch <just.niek@gmail.com>
2017-07-27 15:30:02 +0200

    feat(*): support mitmproxy 2 and add path parameter

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Mitmproxy map local directory script
-This mitmproxy 0.18+ script will map requests to files on the local file system.
+This mitmproxy 2.0.2+ script will map requests to files on the local file system.
 
 ## Example
 When running mitmproxy with the following arguments:
 
 ```bash
-mitmproxy --ignore '^(?!myproject\.localhost)' -s "/path/to/map_local_directory.py /projects/myproject"
+mitmproxy --ignore '^(?!myproject\.localhost)' -s "/path/to/map_local_directory.py --path /url/to/statics/directory --src /projects/myproject/statics"
 ```
 
-Requests like `http://myproject.localhost/somedir/somefile.js` will be mapped to `/projects/myproject/somedir/somefile.js`.
+Requests like `http://myproject.localhost/url/to/statics/directory/js/somefile.js` will be mapped to `/projects/myproject/statics/js/somefile.js`.
 Any request that does not match the domain or with a file on the local file system will pass through unchanged.
--- map_local_directory.py
+++ map_local_directory.py
@@ -1,29 +1,37 @@
-from os.path import isfile
+import os
 import argparse
 import mimetypes
 import urllib
 
 class Replacer:
-    def __init__(self, src):
+    def __init__(self, src, path):
         self.src = src
+        self.path = path
 
     def request(self, flow):
         flow.request.anticache()
         flow.request.anticomp()
 
     def response(self, flow):
-        filepath = self.src + flow.request.path
-        if isfile(filepath):
-            url = urllib.pathname2url(filepath)
+        relative_filepath = remove_prefix(flow.request.path.strip("/"), self.path.strip("/")).strip("/")
+        filepath = os.path.join(self.src, relative_filepath)
+        if os.path.isfile(filepath):
+            url = urllib.request.pathname2url(filepath)
             mimetype = mimetypes.guess_type(url)[0]
-            with open(filepath) as f:
-                flow.response.content = f.read()
+            with open(filepath, "r", encoding="utf-8", errors="ignore") as f:
+                flow.response.content = f.read().encode("utf-8")
                 flow.response.status_code = 200
-                flow.response.reason = 'OK'
-                flow.response.headers['Content-Type'] = mimetype if mimetype is not None else 'text/plain; charset=utf-8'
+                flow.response.reason = "OK"
+                flow.response.headers["Content-Type"] = mimetype if mimetype is not None else "text/plain; charset=utf-8"
+
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
 
 def start():
     parser = argparse.ArgumentParser()
-    parser.add_argument('src', type=str)
+    parser.add_argument("--path", type=str, default="")
+    parser.add_argument("--src", type=str, default=".")
     args = parser.parse_args()
-    return Replacer(args.src)
+    return Replacer(args.src, args.path)
